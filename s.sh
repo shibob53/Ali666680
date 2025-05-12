d2e0285a37fa43480389b1f779df93f8f1815bf4
shibob53 <100147886+shibob53@users.noreply.github.com>
2025-05-12 05:34:40 +0300

    Update s.sh

--- s.sh
+++ s.sh
@@ -2,13 +2,22 @@
 # exit on errorset -o errexit
 
 npm install
+npm run build # uncomment if required
+
+# Store/pull Puppeteer cache with build cache
+
+
+#!/usr/bin/env bash
+set -e
+
+#npm install
 # npm run build # uncomment if required
 
 # Store/pull Puppeteer cache with build cache
-if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then 
+if [[ ! -d "$PUPPETEER_CACHE_DIR" ]]; then 
   echo "...Copying Puppeteer Cache from Build Cache" 
-  cp -R $XDG_CACHE_HOME/puppeteer/ $PUPPETEER_CACHE_DIR
+  cp -R "$XDG_CACHE_HOME/puppeteer/" "$PUPPETEER_CACHE_DIR"
 else 
   echo "...Storing Puppeteer Cache in Build Cache" 
-  cp -R $PUPPETEER_CACHE_DIR $XDG_CACHE_HOME
+  cp -R "$PUPPETEER_CACHE_DIR" "$XDG_CACHE_HOME"
 fi
