57717a5577dc66037750fff7679c9af90041d717
shibob53 <100147886+shibob53@users.noreply.github.com>
2025-05-12 06:09:04 +0300

    render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -22,9 +22,9 @@ npx @puppeteer/browsers install chrome@136.0.7103.92
 # نقل أو نسخ الكاش بين build cache و Puppeteer cache
 if [[ ! -d "$PUPPETEER_CACHE_DIR" ]]; then
   echo ">> نسخ كاش Puppeteer من build cache"
-  cp -R /opt/render/project/src/.cache/puppeteer/chrome/ "$PUPPETEER_CACHE_DIR"
+  cp -R /opt/render/project/src/chrome/linux-136.0.7103.92/chrome-linux64/chrome "$PUPPETEER_CACHE_DIR"
 else
   echo ">> حفظ كاش Puppeteer في build cache"
-  mkdir -p /opt/render/project/src/.cache/puppeteer/chrome/
-  cp -R "$PUPPETEER_CACHE_DIR" /opt/render/project/src/.cache/puppeteer/chrome/
+  mkdir -p /opt/render/project/src/chrome/linux-136.0.7103.92/chrome-linux64/chrome
+  cp -R "$PUPPETEER_CACHE_DIR" /opt/render/project/src/chrome/linux-136.0.7103.92/chrome-linux64/chrome
 fi
