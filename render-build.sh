794542dd691410800be2c80c3219f87db3872ce0
shibob53 <100147886+shibob53@users.noreply.github.com>
2025-05-12 06:41:37 +0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -5,7 +5,6 @@ set -o errexit
 
 echo ">> تثبيت التبعيات"
 npm install
-npx @puppeteer/browsers install chrome@136.0.7103.92
 
 # فك التعليق إذا كان مشروعك يحتاج بناء بعد التثبيت
 # echo ">> بناء المشروع"
@@ -15,15 +14,16 @@ npx @puppeteer/browsers install chrome@136.0.7103.92
 PUPPETEER_CACHE_DIR=/opt/render/.cache/puppeteer
 mkdir -p $PUPPETEER_CACHE_DIR
 
+npx @puppeteer/browsers install chrome@136.0.7103.92
+
 
 echo ">> تثبيت Chrome الخاص بـ Puppeteer"
 #npx puppeteer browsers install chrome
-
 if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then
 echo "...Copying Puppeteer Cache from Build Cache"
 # Copying from the actual path where Puppeteer stores its Chrome binary
-cp -R /opt/render/project/src/chrome/linux-136.0.7103.92/chrome-linux64/chrome/ $PUPPETEER_CACHE_DIR
+cp -R /opt/render/project/src/.cache/puppeteer/chrome/ $PUPPETEER_CACHE_DIR
 else
 echo "...Storing Puppeteer Cache in Build Cache"
-cp -R $PUPPETEER_CACHE_DIR /opt/render/project/src/chrome/linux-136.0.7103.92/chrome-linux64/chrome/
+cp -R $PUPPETEER_CACHE_DIR /opt/render/project/src/.cache/puppeteer/chrome/
 fi
